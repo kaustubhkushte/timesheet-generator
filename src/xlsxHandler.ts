9f59f788c5007d624fa9cd79116d1c82c722ad14
Kaustubh Kushte <kaustubh.kushte.jc@joshsoftware.digital>
2023-07-25 18:05:05 +0530

    credentials removed

--- src/xlsxHandler.ts
+++ src/xlsxHandler.ts
@@ -2,6 +2,41 @@ import * as XLSX from 'xlsx';
 import { EmployeeData } from './models/EmployeeData';
 import { TimesheetData } from './models/TimesheetData';
 import { EmployeeTimesheetData } from './models/EmployeeTimesheetData';
+import exp from 'constants';
+
+function calculateStartEndDate(referenceDateStr:string){
+  
+// Split the date string
+const dateParts = referenceDateStr.split('-');
+
+// Extract day, month, and year from the date parts
+const day = parseInt(dateParts[0], 10);
+const month = parseInt(dateParts[1], 10) - 1; // Adjust month to 0-based index
+const year = parseInt(dateParts[2], 10);
+
+// Create the reference date object
+const referenceDate = new Date(year, month, day);
+
+// Get the year and month from the reference date
+const referenceYear = referenceDate.getFullYear();
+const referenceMonth = referenceDate.getMonth();
+
+// Calculate the start date of the month
+const startDate = new Date(referenceYear, referenceMonth, 1);
+const startDateString = `${startDate.getFullYear()}-${('0' + (startDate.getMonth() + 1)).slice(-2)}-${('0' + startDate.getDate()).slice(-2)}`;
+
+// Calculate the end date of the month
+const endDate = new Date(referenceYear, referenceMonth + 1, 0);
+const endDateString = `${endDate.getFullYear()}-${('0' + (endDate.getMonth() + 1)).slice(-2)}-${('0' + endDate.getDate()).slice(-2)}`;
+
+// Print the start and end dates
+console.log('Start Date:', startDateString);
+console.log('End Date:', endDateString);
+return {
+  "startDate":startDate,
+  "endDate":endDate
+}
+}
 
 function evaluateDay(fromDate: string): string {  
   const [day, month, year] = fromDate.split('-').map(Number);
@@ -13,6 +48,54 @@ function evaluateDay(fromDate: string): string {
   return dayOfWeek;
 }
 
+export function addMissingDaysData(timesheetData:TimesheetData[]){
+  // Extract the unique dates from the CSV data
+  const existingDates = [...new Set(timesheetData.map((data) => data.date))];
+  const monthDates = calculateStartEndDate(existingDates[0]);
+  const startDate = monthDates.startDate;
+  const endDate = monthDates.endDate;
+  // Get all dates for the month
+  const allDates: string[] = [];
+  for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
+    const year = date.getFullYear();
+  const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
+  const dateString = `${day}-${month}-${year}`;
+    
+    allDates.push(dateString);
+  }
+  
+  // Find the missing dates
+  const missingDates = allDates.filter((date) => !existingDates.includes(date));
+
+  // Generate default data for missing dates
+  const blankData = missingDates.map((date) => ({
+    date,
+    day:evaluateDay(date),
+    task: '',
+    hrs: 0,
+  }));
+
+// Combine the existing data with the default data
+const updatedData = [...timesheetData, ...blankData];
+
+
+// Sort the data by date
+updatedData.sort((a, b) => {
+  const datePartsA = a.date.split('-');
+  const datePartsB = b.date.split('-');
+
+  // Convert the date parts to yyyy-mm-dd format for comparison
+  const dateA = new Date(`${datePartsA[2]}-${datePartsA[1]}-${datePartsA[0]}`);
+  const dateB = new Date(`${datePartsB[2]}-${datePartsB[1]}-${datePartsB[0]}`);
+
+  return dateA.getTime() - dateB.getTime();
+});
+
+  return updatedData;
+}
+
+
 export function groupByEmployeeName(data: EmployeeData[]): EmployeeTimesheetData[] {
   
   const groupedData: Record<string, TimesheetData[]> = {};
@@ -23,8 +106,7 @@ export function groupByEmployeeName(data: EmployeeData[]): EmployeeTimesheetData
     if (!groupedData[employeeName]) {
       groupedData[employeeName] = [];
     }
-    const day = evaluateDay(date); 
-      
+    const day = evaluateDay(date);      
     groupedData[employeeName].push({ date, day, task, hrs });
   });
 
@@ -32,9 +114,12 @@ export function groupByEmployeeName(data: EmployeeData[]): EmployeeTimesheetData
 
   for (const employeeName in groupedData) {
     const timesheetData:TimesheetData[] = groupedData[employeeName];
+
+    const updatedData = addMissingDaysData(timesheetData);
+
     const employeeTimesheet:EmployeeTimesheetData = {
       employeeName:employeeName,
-      data:timesheetData
+      data:updatedData
     }
     employeeTimesheetArray.push(employeeTimesheet);
   }
@@ -75,8 +160,8 @@ export function createWorkbook(employeeTimesheetArray: EmployeeTimesheetData[]):
     worksheet['!cols'] = [
       { width: 15 },
       { width: 20 },
-      { width: 100 },
-      { width: 50 }
+      { width: 30 },
+      { width: 15 }
     ];
     XLSX.utils.book_append_sheet(workbook, worksheet, employeeTimesheet.employeeName);
 
@@ -86,6 +171,8 @@ export function createWorkbook(employeeTimesheetArray: EmployeeTimesheetData[]):
   return workbook;
 }
 
+
+
 export function readExcelFile(filePath: string): Promise<EmployeeData[]> {
   return new Promise((resolve, reject) => {
     try {
@@ -94,7 +181,7 @@ export function readExcelFile(filePath: string): Promise<EmployeeData[]> {
       
       const jsonArray = XLSX.utils.sheet_to_json(worksheet, {        
         defval: undefined,
-        raw: false,
+        raw: true,
         dateNF: 'yyyy-mm-dd',
       });
 
